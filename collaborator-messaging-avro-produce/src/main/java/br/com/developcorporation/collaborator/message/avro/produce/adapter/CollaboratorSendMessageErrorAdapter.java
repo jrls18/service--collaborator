a6a6f36ba037021b63f703734dd84d4ad3796301
Rede Espaco Saboroso <redeespacosaboroso@example.com>
2022-07-09 22:58:35 -0300

    Criado no produce para enviar mensagens

--- collaborator-messaging-avro-produce/src/main/java/br/com/developcorporation/collaborator/message/avro/produce/adapter/CollaboratorSendMessageErrorAdapter.java
+++ collaborator-messaging-avro-produce/src/main/java/br/com/developcorporation/collaborator/message/avro/produce/adapter/CollaboratorSendMessageErrorAdapter.java
@@ -1,4 +1,24 @@
 package br.com.developcorporation.collaborator.message.avro.produce.adapter;
 
-public class CollaboratorSendMessageErrorAdapter {
+import br.com.developcorporation.collaborator.domain.exception.DomainException;
+import br.com.developcorporation.collaborator.domain.port.CollaboratorSendMessageErrorPort;
+import br.com.developcorporation.collaborator.message.avro.produce.mapper.CollaboratorSendMessageErrorMapper;
+import br.com.developcorporation.collaborator.message.avro.produce.service.CollaboratorSendMessageErrorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Service
+public class CollaboratorSendMessageErrorAdapter implements CollaboratorSendMessageErrorPort {
+
+    private final CollaboratorSendMessageErrorService service;
+
+    @Override
+    public void send(DomainException dto) {
+        if(Objects.nonNull(dto)){
+            service.sendMassage(CollaboratorSendMessageErrorMapper.INSTANCE.toAvro(dto));
+        }
+    }
 }
