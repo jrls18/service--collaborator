64e45f0ceace6502a1f48219cdc32011fd90982d
John <john.john16@hotmail.com>
2022-12-13 20:02:31 -0300

    ajuste

--- collaborator-e2e/src/test/java/br/com/developcorporation/collaborator/e2e/RunCucumberTest.java
+++ collaborator-e2e/src/test/java/br/com/developcorporation/collaborator/e2e/RunCucumberTest.java
@@ -2,15 +2,13 @@ package br.com.developcorporation.collaborator.e2e;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.Ignore;
+
 import org.junit.runner.RunWith;
 
-
-//@Ignore
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/e2e"},
-        plugin = {"pretty", "json:target/cucumber.json"},
+        plugin = {"pretty", "json:target/cucumber-reports/cucumber.json"},
         glue = {"br.com.developcorporation.collaborator.e2e"},
         tags = "@geral",
         monochrome = true
