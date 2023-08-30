fd843d72c2c58052cc2333f7f3aab71e262948fe
John <john.john16@hotmail.com>
2023-08-30 14:23:51 -0300

    ajuste no serialize

--- collaborator-domain/src/main/java/br/com/group/developer/corporation/service/collaborator/domain/exception/CompanyInternalServerErrorException.java
+++ collaborator-domain/src/main/java/br/com/group/developer/corporation/service/collaborator/domain/exception/CompanyInternalServerErrorException.java
@@ -1,6 +1,8 @@
 package br.com.group.developer.corporation.service.collaborator.domain.exception;
 
 public class CompanyInternalServerErrorException extends RuntimeException {
+    private static final long serialVersionUID = 7677109049488920793L;
+
     public CompanyInternalServerErrorException(final String message){
         super(message);
     }
--- collaborator-domain/src/main/java/br/com/group/developer/corporation/service/collaborator/domain/exception/InternalServerErrorException.java
+++ collaborator-domain/src/main/java/br/com/group/developer/corporation/service/collaborator/domain/exception/InternalServerErrorException.java
@@ -1,6 +1,8 @@
 package br.com.group.developer.corporation.service.collaborator.domain.exception;
 
 public class InternalServerErrorException extends RuntimeException {
+    private static final long serialVersionUID = -7501314447979886448L;
+
     public InternalServerErrorException(final String message){
         super(message);
     }
