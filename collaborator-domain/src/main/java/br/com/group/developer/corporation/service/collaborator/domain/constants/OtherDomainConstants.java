90d660a4d73fb28e33daa0903e4310d10ae9ef82
John <john.john16@hotmail.com>
2023-08-30 14:36:40 -0300

    removendo field nao usados

--- collaborator-domain/src/main/java/br/com/group/developer/corporation/service/collaborator/domain/constants/OtherDomainConstants.java
+++ collaborator-domain/src/main/java/br/com/group/developer/corporation/service/collaborator/domain/constants/OtherDomainConstants.java
@@ -6,13 +6,9 @@ public final class OtherDomainConstants {
     }
 
     public static final long EMAIL = 1L;
-
     public static final long PENDENTE_VALIDACAO_DE_EMAIL = 4L;
-
     public static final long ATIVO = 1;
-
     public static final long IMAGE_PROFILE = 1L;
-
     public static final String SAVE = "I";
     public static final String DELETE_FILE = "D";
 }
